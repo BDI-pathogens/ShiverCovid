29747f99f5d0894b9811678a836dd3cbc18c3a83
Laura Thomson <lrthmsn@gmail.com>
2021-10-18 11:49:52 +0100

    Deal with BrokenPipeError

--- snakemake/scripts/pipeline/preprocess_readnames.py
+++ snakemake/scripts/pipeline/preprocess_readnames.py
@@ -2,12 +2,23 @@ from sys import stdout, argv
 
 
 def format_file(in_file, tag):
-    with open(in_file, 'r') as f_in:
-        for count, value in enumerate(f_in):
-            if not count % 4:
-                stdout.write('{0}/{1}\n'.format(value.split()[0].rsplit('/', 1)[0], tag))
-            else:
-                stdout.write(value)
+    try:
+        with open(in_file, 'r') as f_in:
+            for count, value in enumerate(f_in):
+                if not count % 4:
+                    stdout.write('{0}/{1}\n'.format(value.split()[0].rsplit('/', 1)[0], tag))
+                else:
+                    stdout.write(value)
+        # https://docs.python.org/3/library/signal.html#note-on-sigpipe
+        # flush output here to force SIGPIPE to be triggered
+        # while inside this try block.
+        sys.stdout.flush()
+    except BrokenPipeError:
+        # Python flushes standard streams on exit; redirect remaining output
+        # to devnull to avoid another BrokenPipeError at shutdown
+        devnull = os.open(os.devnull, os.O_WRONLY)
+        os.dup2(devnull, sys.stdout.fileno())
+        sys.exit(1)  # Python exits with error code 1 on EPIPE
 
 
 def main():
