f340f6584099ba8fb05572a5113cbb3c13d8e01a
Laura Thomson <lrthmsn@gmail.com>
2021-11-22 11:03:18 +0200

    Add SMARTER_ADAPTER_KIT

--- scripts/preprocessing/setup.sh
+++ scripts/preprocessing/setup.sh
@@ -3,14 +3,20 @@
 set -e
 
 RAW_DATA_DIR="${1}"
+SMARTER_ADAPTER_KIT="${2}"
 REPO_BASE_DIR="$(dirname "$(dirname "$(dirname "$(readlink -f -- "$0")")")")"
 
 SCRIPT_UPDATE_PANGOLIN="${REPO_BASE_DIR}/scripts/preprocessing/pangolin_update.sh"
 
+usage() {
+  echo "Usage: $0 RAW_DATA_DIR SMARTER_ADAPTER_KIT"
+  echo "* RAW_DATA_DIR: The directory containing the raw data"
+  echo "* SMARTER_ADAPTER_KIT: The SMARTer Adapter kit used by the lab [v2, v3]"
+  exit 1
+}
+
 check_input() {
-  if [[ -z "${RAW_DATA_DIR}" ]]; then
-    echo "Usage: $0 <Raw Data Directory>"
-  fi
+  [[ -z ${RAW_DATA_DIR} || -z ${SMARTER_ADAPTER_KIT} ]] && { usage; }
 }
 
 update_pangolin() {
@@ -36,9 +42,11 @@ check_input
   trap 'error $?' EXIT
   msg="INFO: Raw Data Directory: ${RAW_DATA_DIR}"
   echo "${msg}"
+  msg="INFO: SMARTer Adapter Kit: ${SMARTER_ADAPTER_KIT}"
+  echo "${msg}"
   msg="INFO: Preparing Snakemake config for processing"
   echo "${msg}"
   cd "${REPO_BASE_DIR}/snakemake"
-  ./prepare.sh "${RAW_DATA_DIR}"
+  ./prepare.sh "${RAW_DATA_DIR}" "${SMARTER_ADAPTER_KIT}"
   update_pangolin
 }
